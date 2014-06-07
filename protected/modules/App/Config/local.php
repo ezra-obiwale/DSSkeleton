462a4c04c787aefa1811d0e8c03f0f32ee120c9f
ezra <ezra@ezra-mach>
2014-06-07 15:14:48 +0100

    Added local config file to App module

---
+++ protected/modules/App/Config/local.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'autoload' => array(
+        'files' => array( // array of path to files within the module
+        ),
+        'dirs' => array( // array of path of directories containing php files to autoload
+        ),
+    ),
+);
